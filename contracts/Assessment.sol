40a99bea4260563ea520469581540fc881542586
Eric <155745830+ericevangelista21@users.noreply.github.com>
2024-01-06 23:14:24 +0800

    updated

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,46 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
+    uint256 public nikePrice = 100;
+    uint256 public adidasPrice = 80;
+
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event ShoeBought(string brand, uint256 price);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +41,23 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function buyShoe(string memory brand) public {
+        uint256 shoePrice;
+        if (keccak256(abi.encodePacked(brand)) == keccak256(abi.encodePacked("nike"))) {
+            shoePrice = nikePrice;
+        } else if (keccak256(abi.encodePacked(brand)) == keccak256(abi.encodePacked("adidas"))) {
+            shoePrice = adidasPrice;
+        } else {
+            revert("Invalid brand");
+        }
+
+        require(balance >= shoePrice, "Insufficient funds to buy shoe");
+        balance -= shoePrice;
+        emit ShoeBought(brand, shoePrice);
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect, useCallback } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,111 +7,248 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [shoePrice, setshoePrice] = useState("");
+  const [depositAmount, setDepositAmount] = useState(0);
+  const [withdrawalAmount, setWithdrawalAmount] = useState(1);
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
-      handleAccount(account);
+      const accounts = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(accounts[0]);
     }
-  }
+  };
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected: ", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
-  }
+  };
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
+      alert("MetaMask wallet is required to connect");
       return;
     }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
-    handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
+    const accounts = await ethWallet.request({
+      method: "eth_requestAccounts",
+    });
+    handleAccount(accounts[0]);
+
+    // Once the wallet is set, we can get a reference to our deployed contract
     getATMContract();
   };
 
-  const getATMContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethWallet);
-    const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
+  const getATMContract = useCallback(() => {
+    if (ethWallet) {
+      const provider = new ethers.providers.Web3Provider(ethWallet);
+      const signer = provider.getSigner();
+      const atmContract = new ethers.Contract(
+        contractAddress,
+        atmABI,
+        signer
+      );
 
-  const getBalance = async() => {
-    if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+      setATM(atmContract);
     }
-  }
+  }, [ethWallet]);
 
-  const deposit = async() => {
+  const getBalance = async () => {
     if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
+      try {
+        const currentBalance = await atm.getBalance();
+        console.log("Current Balance:", currentBalance.toNumber());
+        setBalance(currentBalance.toNumber());
+      } catch (error) {
+        console.error("Error fetching balance:", error);
+      }
     }
-  }
+  };
 
-  const withdraw = async() => {
+  const deposit = async (amount) => {
     if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
-      getBalance();
+      try {
+        let tx = await atm.deposit(amount);
+        await tx.wait();
+        getBalance();
+      } catch (error) {
+        console.error("Error in deposit transaction:", error.message);
+        alert(`Failed to deposit: ${error.message}`);
+      }
     }
-  }
+  };
+
+  const withdraw = async () => {
+    if (atm) {
+      try {
+        let tx = await atm.withdraw(withdrawalAmount);
+        await tx.wait();
+        getBalance();
+      } catch (error) {
+        console.error("Error in withdraw transaction:", error.message);
+        alert(`Failed to withdraw: ${error.message}`);
+      }
+    }
+  };
+
+  const buyShoe = useCallback(async () => {
+    if (atm && shoePrice) {
+      try {
+        let tx = await atm.buyShoe(shoePrice);
+        await tx.wait();
+        getBalance();
+      } catch (error) {
+        console.error("Error in buyShoe transaction:", error.message);
+        alert(`Failed to buy ticket: ${error.message}`);
+      }
+    }
+  }, [atm, shoePrice]);
 
   const initUser = () => {
-    // Check to see if user has Metamask
+    // Check to see if the user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <p>Please install Metamask to use this Plane.</p>;
     }
 
-    // Check to see if user is connected. If not, connect to their account
+    // Check to see if the user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return (
+        <button onClick={connectAccount}>
+          Please connect your Metamask wallet
+        </button>
+      );
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
     }
 
     return (
       <div>
+        <p>NIKE - 100 ETH</p>
+        <p>ADIDAS - 80 ETH</p>
         <p>Your Account: {account}</p>
         <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <div>
+          <label>Select Shoe Brand:</label>
+          <div className="input-container">
+            <select
+              value={shoePrice}
+              onChange={(e) => setshoePrice(e.target.value)}
+            >
+              <option value="nike">Nike</option>
+              <option value="adidas">Adidas</option>
+            </select>
+            <button onClick={buyShoe}>Buy</button>
+          </div>
+        </div>
+    
+        <div>
+          <label>Deposit Amount (ETH):</label>
+          <div className="input-container">
+            <input
+              type="number"
+              min="0"
+              step="0.1"
+              value={depositAmount}
+              onChange={(e) => setDepositAmount(e.target.value)}
+            />
+            <button onClick={() => deposit(depositAmount)}>Deposit</button>
+          </div>
+        </div>
+        <div>
+          <label>Withdrawal Amount (ETH):</label>
+          <div className="input-container">
+            <input
+              type="number"
+              min="1"
+              value={withdrawalAmount}
+              onChange={(e) => setWithdrawalAmount(e.target.value)}
+            />
+            <button onClick={withdraw}>Withdraw</button>
+          </div>
+        </div>
       </div>
-    )
-  }
+    );
+  };
 
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  useEffect(() => {
+    getATMContract();
+  }, [getATMContract]);
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header>
+        <img
+          src="https://t3.ftcdn.net/jpg/01/30/14/60/360_F_130146011_64algBC3Kz91zzdht7qeW2fEnUipEtwB.jpg"
+          alt="Metacrafters Logo"
+          style={{ width: "50%", display: "block", margin: "0 auto" }}
+        />
+        <h1>Welcome to Shoe Express!</h1>
+      </header>
       {initUser()}
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
+          margin: 50px;
+          padding: 50px;
+          border: 1px solid #ddd;
+          border-radius: 8px;
+          background-color: white;
+          max-width: 950px;
+          margin: auto;
+          font-family: 'Helvetica';
+          color: black;
         }
-      `}
-      </style>
+  
+        label {
+          display: block;
+          margin-bottom: 5px;
+        }
+  
+        select,
+        input {
+          width: 100%;
+          padding: 8px;
+          margin-bottom: 10px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          box-sizing: border-box;
+        }
+  
+        .input-container {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 15px;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+          margin: 5px;
+        }
+  
+        button:hover {
+          background-color: #45a049;
+        }
+      `}</style>
     </main>
-  )
+  );
 }
